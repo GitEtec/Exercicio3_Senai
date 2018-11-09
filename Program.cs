a3ede7d5ec3f87a33865a052cb3b642271f1c93f
HypnosProxy <debora.pyhp@gmail.com>
2018-11-09 20:51:35 -0200

    3ºExericio Senai

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConversãoTempFpC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercicio Senai 3
+              Efetue a leitura de uma temperatura medida em graus Fahrenheit
+              e apresente em Celsius.*/
+            int temp_F,temp_C;
+            Console.Write("Temperatura em Fº(fahrenheit): ");
+            temp_F = int.Parse(Console.ReadLine());
+            temp_C = ((temp_F - 32) * 5) / 9;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Temperatura em Celsius: " + temp_C + "C°");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+            Console.Write("Tecle <enter> para sair");
+            Console.ReadLine();
+        }   
+    }
+}
